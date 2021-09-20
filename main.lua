0313ed566888ced27c29c923ac42af094d3d9b93
HCRJeewantha <ravindujxyz@gmail.com>
2021-09-20 22:18:30 +0530

    implimented basic chart with all logics

--- main.lua
+++ main.lua
@@ -12,14 +12,14 @@ local density_air = 1.275
 local drag_co = 0.5
 local gravity_force = -9.81 * mass
 
-local angle = 42
-local init_velocity = 4.46944
+local angle = 45
+local init_velocity = 4
 
--- local init_velocity_x = init_velocity * math.cos(angle)
--- local init_velocity_y = init_velocity * math.sin(angle)
+local init_velocity_x = init_velocity * math.cos(angle)
+local init_velocity_y = init_velocity * math.sin(angle)
 
-local init_velocity_x = 4.46944
-local init_velocity_y = 0.0
+-- local init_velocity_x = 4.46944
+-- local init_velocity_y = 0.0
 
 local delta_time = 0.01
 
@@ -72,8 +72,12 @@ local velocity_y
 local position_x = 0
 local position_y = 0
 
-for i = 1, 12, 1 do
+local min = 0
+local max = 100
+local i = 1
 
+while(position_y >= 0) 
+do
 	--X direction
 	--calculate drag force
 	drag_force_x = -dragForceCalculate(init_velocity_x)
@@ -112,18 +116,72 @@ for i = 1, 12, 1 do
 
 	--store x cordinates for create chart
 	y_cordinates[i] = position_y
+	print(position_y)
 
 	--calculate position
 	position_y =  calcPosition(init_velocity_y, acceleration_y, delta_time) + position_y
 
 	--final velosity == initial velocity
 	init_velocity_y = velocity_y
-
-
-
-
-    print( "vel", y_cordinates[i])
+	i = i + 1
 end
+
+
+function findMinAndMax(table)
+	local key_max, max = 1, table[1]
+	local key_min, min = 1, table[1]
+
+	for k, v in ipairs(table) do
+	    if table[k] > max then
+	        key_max, max = k, v
+	    end
+	    if table[k] < min then
+	        key_min, min = k, v
+	    end
+	end
+
+	return min, max
+end
+
+local x_normalized_cordinates = {}
+local y_normalized_cordinates = {}
+
+--this function will normalized the data
+function normalizingData()
+	
+	--normalizing
+	x_min, x_max = findMinAndMax(x_cordinates)
+	y_min, y_max = findMinAndMax(y_cordinates)
+
+	for k, v in ipairs(x_cordinates) do
+		x_normalized_cordinates[k] = 50 + (v - x_min) / (x_max - x_min) * 430 --zi = (xi – min(x)) / (max(x) – min(x)) * 300
+	end
+
+	for k, v in ipairs(y_cordinates) do
+		y_normalized_cordinates[k] = 290 - (v - y_min) / (y_max - y_min) * 250 --zi = (xi – min(x)) / (max(x) – min(x)) * 300
+	end
+
+end
+
+normalizingData()
+	
+
+local line = display.newLine( 0, 0, 0, 0 )
+-- star:append( 305,165, 243,216 )
+
+for k, v in ipairs(y_normalized_cordinates) do
+
+	line:append( x_normalized_cordinates[k],y_normalized_cordinates[k] )
+end
+line:setStrokeColor( 0, 1, 0, 1 )
+line.strokeWidth = 3
+
+local chart = display.newLine( 50, 290, 50, 10 )
+chart:append( 50,290, 500, 290 )
+
+chart:setStrokeColor( 1, 0, 0, 1 )
+chart.strokeWidth = 3
+
 
 -- Set default screen background color to blue
 display.setDefault( "background", 1, 1, 1 )
